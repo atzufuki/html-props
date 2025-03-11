73fc6f9fc345ed59e2696abdd6b195bafb78ef50
atzufuki <30522886+atzufuki@users.noreply.github.com>
2025-03-11 14:03:18 +0200

    Add workflows (#6)
    * Add workflows
    
    * Add lint rules
    
    * Add explicit return types

--- src/jsx/jsx-runtime.ts
+++ src/jsx/jsx-runtime.ts
@@ -1,19 +1,15 @@
-// deno-lint-ignore-file no-explicit-any
-
 type Constructor<T> = new (...args: any[]) => T;
 
-declare global {
-  var JSX: {
-    createElement: (type: Constructor<any> | string, props: any, ...children: any[]) => any;
-  };
-}
-
-globalThis.JSX = {
-  createElement: (type: Constructor<any> | string, props: any, ...children: any[]) => {
+export const JSX = {
+  createElement: (
+    type: Constructor<HTMLElement> | string,
+    props: {} | null,
+    ...children: HTMLElement[][]
+  ): HTMLElement | string => {
     if (typeof type === 'function') {
       return new type({ ...props, children: children.flat() });
     }
 
-    return { type, props, children };
+    return '';
   },
 };
