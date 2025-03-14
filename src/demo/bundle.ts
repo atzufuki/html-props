f03a65c007a3f8a9d5887219081a0fa1be7cd2fb
atzufuki <30522886+atzufuki@users.noreply.github.com>
2025-03-14 17:26:50 +0200

    Change bundling

--- src/demo/bundle.ts
+++ src/demo/bundle.ts
@@ -3,7 +3,7 @@ import { denoPlugins } from 'jsr:@luca/esbuild-deno-loader';
 
 await esbuild.build({
   plugins: [...denoPlugins()],
-  entryPoints: ['./src/demo/static/app.tsx'],
+  entryPoints: ['./src/demo/static/app.ts'],
   outfile: './src/demo/static/app.js',
   bundle: true,
   format: 'esm',
