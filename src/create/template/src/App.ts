cf22c30f7ca9b72ab07e2b4465a6e33790b1bac9
atzufuki <30522886+atzufuki@users.noreply.github.com>
2025-12-09 22:53:36 +0200

    Improved template

--- src/create/template/src/App.ts
+++ src/create/template/src/App.ts
@@ -1,55 +1,51 @@
 import { createRef, HTMLPropsMixin } from '@html-props/core';
-import { computed, effect, signal } from '@html-props/signals';
-import { Anchor, Button, Code, Div, Heading1, Image, Paragraph } from '@html-props/built-ins';
+import { signal } from '@html-props/signals';
+import { Anchor, Button, Code, Heading1, Image, Paragraph } from '@html-props/built-ins';
+import { Column, Row } from '@html-props/layout';
 
 export default class App extends HTMLPropsMixin(HTMLElement) {
-  aborter = new AbortController();
   buttonRef = createRef<HTMLButtonElement>();
   count = signal(0);
-  doubleCount = computed(() => this.count() * 2);
 
-  connectedCallback(): void {
-    super.connectedCallback?.();
-
-    const signal = this.aborter.signal;
-
-    effect(() => {
-      const button = this.buttonRef.current;
-      if (button) {
-        button.textContent = `count is ${this.count()}`;
-      }
-    }, { signal });
-  }
-
-  disconnectedCallback(): void {
-    super.disconnectedCallback?.();
-    this.aborter.abort();
+  update() {
+    const button = this.buttonRef.current;
+    if (button) {
+      button.textContent = `count is ${this.count.get()}`;
+    }
   }
 
   render() {
-    return new Div({
+    return new Column({
+      crossAxisAlignment: 'center',
       content: [
-        new Anchor({
-          href: 'https://www.typescriptlang.org/',
-          target: '_blank',
-          content: new Image({
-            src: '/typescript.svg',
-            alt: 'TypeScript logo',
-            className: 'logo',
-          }),
-        }),
-        new Anchor({
-          href: 'https://github.com/atzufuki/html-props',
-          target: '_blank',
-          content: new Image({
-            src: '/html-props.svg',
-            alt: 'HTML Props logo',
-            className: 'logo html-props',
-          }),
+        new Row({
+          mainAxisAlignment: 'center',
+          gap: '2rem',
+          content: [
+            new Anchor({
+              href: 'https://www.typescriptlang.org/',
+              target: '_blank',
+              content: new Image({
+                src: '/typescript.svg',
+                alt: 'TypeScript logo',
+                className: 'logo',
+              }),
+            }),
+            new Anchor({
+              href: 'https://github.com/atzufuki/html-props',
+              target: '_blank',
+              content: new Image({
+                src: '/html-props.svg',
+                alt: 'HTML Props logo',
+                className: 'logo html-props',
+              }),
+            }),
+          ],
         }),
         new Heading1({ textContent: 'TypeScript + HTML Props' }),
-        new Div({
+        new Column({
           className: 'card',
+          crossAxisAlignment: 'center',
           content: [
             new Button({
               ref: this.buttonRef,
